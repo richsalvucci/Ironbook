b0a659c368373bae004cb2775c7ea8322a049bda
Kevin Collins <kevincol54@gmail.com>
2014-07-30 14:26:02 -0400

    added image upload to signup form and integrated dropbox API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :name, :location, :course, :phone, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :name, :location, :course, :phone, :password, :password_confirmation, :image)}
 
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :name, :location, :course, :phone, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :name, :location, :course, :phone, :password, :password_confirmation, :current_password, :image)}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :image
+  has_attached_file :image,
+  :storage => :dropbox,
+    :dropbox_credentials => { app_key: ENV['APP_KEY'],
+                              app_secret: ENV['APP_SECRET'],
+                              access_token: ENV['ACCESS_TOKEN'],
+                              access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+                              user_id: ENV['USER_ID'],
+                              access_type: 'app_folder'}
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
